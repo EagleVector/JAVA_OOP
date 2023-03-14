35ba7b299789edf4531e34f7ff187230da8533e9
EagleVector <kumarsubhambili3@gmail.com>
2023-03-15 00:56:12 +0530

    Introduction to OOP concept

--- src/OOPS/Intro.java
+++ src/OOPS/Intro.java
@@ -4,26 +4,24 @@ import java.util.Arrays;
 
 public class Intro {
     public static void main(String[] args) {
-//        int[] numbers = new int[5];
-//        numbers[3] = 67;
-//        numbers[2] = 65;
-//        numbers[1] = 90;
-//        numbers[0] = 23;
-//        numbers[4] = 8;
-//        System.out.println(Arrays.toString(numbers));
-//
-//        String[] names = {"Cherry", "Sunny", "Bunny", "Puppy", "Kissie"};
-//        System.out.println(Arrays.toString(names));
-// Reversing a string program.
-//        String str = "Give it a try";
-//        System.out.println(str);
-//        String[] arr = str.split(" ");
-//        int start = 0;
-//        int end = arr.length - 1;
-//        for (int i = end; i >= start; i--) {
-//            System.out.print(arr[i] + " ");
-//        }
+        int[] numbers = new int[5];
+        numbers[3] = 67;
+        numbers[2] = 65;
+        numbers[1] = 90;
+        numbers[0] = 23;
+        numbers[4] = 8;
+        System.out.println(Arrays.toString(numbers));
 
-
+        String[] names = {"Cherry", "Sunny", "Bunny", "Puppy", "Kissie"};
+        System.out.println(Arrays.toString(names));
+ // Reversing a string program.
+        String str = "Give it a try";
+        System.out.println(str);
+        String[] arr = str.split(" ");
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i = end; i >= start; i--) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
